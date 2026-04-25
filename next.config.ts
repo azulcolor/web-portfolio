83ab1e5c04fad863b4d1e6d3e3696eb1c8d7a982
Pastelin <lealpastelin@gmail.com>
2026-04-25 14:55:37 -0600

    optimazing

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,12 @@ const nextConfig: NextConfig = {
   transpilePackages: [],
   compiler: {
   },
-  images: {
-    formats: ["image/avif", "image/webp"],
-    qualities: [75, 95, 100],
-    deviceSizes: [400, 640, 750, 1080, 1200, 1920],
-    imageSizes: [64, 128, 256, 384],
-  },
+images: {
+  formats: ["image/avif", "image/webp"],
+  qualities: [75, 95, 100],
+  deviceSizes: [400, 640, 728, 750, 1080, 1200, 1920],
+  imageSizes: [64, 128, 256, 384],
+},
 };
 
 export default nextConfig;
